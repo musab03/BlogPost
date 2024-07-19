5ba1624d65fda5817bd658f2e2b00af1d093cbfc
musab03 <musab.khurram@gmail.com>
2024-07-19 22:20:23 +0500

    add: main file

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import authService from './appwrite/auth'
+import './App.css'
+import { useDispatch } from 'react-redux'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
+
+
+
+function App() {
+  const [loading, setloading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setloading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex  justify-center bg-gray-400'>
+      <div className='w-full '>
+        <Header/>
+        <main>
+        TODO  {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
+}
+
+export default App
