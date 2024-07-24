53e4bbd4ec955710d451bf32d31fcccbf70102c9
musab03 <musab.khurram@gmail.com>
2024-07-24 07:26:14 +0500

    add: postcard component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import appwriteService from '../appwrite/config_auth'
 import { Link } from 'react-router-dom'
 
-function PostCard({$id, title, featureImage }) {
+function PostCard({$id, title, featuredImage }) {
+
+   
   return (
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featureImage)} alt={title} className='rounded-xl' />
+                <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='rounded-xl' />
             </div>
             <h2 className='text-2xl font-bold  '>{title}</h2>
         </div>
