3cfa097f47d0c5e185e671179af575b757ed5541
musab03 <musab.khurram@gmail.com>
2024-07-24 07:11:48 +0500

    fix: button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 
-function Button({
+export default function Button({
     children,
     type ='button',
-    bgColor = 'bg-blue-600',
+    bgColor ,
     textColor = 'text-white',
     className = '',
     ...props
 }) {
+
+  const defaultBgColor = 'bg-blue-600'; // Default background color
+  const appliedBgColor = bgColor !== undefined ? bgColor : defaultBgColor;
+
   return (
-    <button className={`px-4 py-2 rounded-lg ${className} ${bgColor} ${textColor} `} {...props}>
+    <button type={type} className={`px-4 py-2 rounded-lg  ${className} ${appliedBgColor} ${textColor}`} {...props}>
         {children}
     </button> 
  )
 }
 
-export default Button
