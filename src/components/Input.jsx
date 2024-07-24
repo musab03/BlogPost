232159be4383387863f979a5c314932b8f381250
musab03 <musab.khurram@gmail.com>
2024-07-24 07:11:10 +0500

    fix: input form

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,11 +7,11 @@ const Input = React.forwardRef(function Input(
   const id = useId();
   return (
     <div className="w-full">
-      {label && (
+      {label && 
         <label className="inline-block mb-1 pl-1" htmlFor={id}>
           {label}
         </label>
-      )}
+      }
       <input
         type={type}
         className={` px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
